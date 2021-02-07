2676bb8523e713b7e2de483e4fd9fca946406941
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2021-02-07 08:44:12 -0500

    this is a public token, but
    for this excersice I want to suppose that this is a secret value, the tracking has the value and this is a security leak... I know

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const config = {
   bioEndpoint: process.env.BIO_ENDPOINT || 'https://bio.torre.co/api/bios',
   torreSearchEndpoint: process.env.TORRE_SEARCH_ENDPOINT || 'https://search.torre.co',
   extractEndpoint: process.env.EXTRACT_ENDPOINT || 'https://monkeylearn.com/word-cloud/api/extract/',
-  monkeylearnToken: process.env.MONKEY_LEARN_TOKEN || 'LwLnTOn367TWl5cUJeQZgpvbzTzXdNOQy6dQ7qRWFJlmuVynZQQYDPEKBQOBMuyg',
+  monkeylearnToken: process.env.MONKEY_LEARN_TOKEN || 'Configure Secret Token!',
 };
 export default config;
